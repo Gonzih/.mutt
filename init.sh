24e5db64bfec912c71f03088faf456cf8cc6c4aa
Max Gonzih <gonzih@gmail.com>
2014-01-06 19:08:45 +0300

    Add urlview to the project

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 ln -s ~/.mutt/muttrc ~/.muttrc
 ln -s ~/.mutt/offlineimap/offlineimaprc ~/.offlineimaprc
 cp ~/.mutt/msmtp/msmtprc ~/.msmtprc
+ln -s ~/.mutt/urlview ~/.urlview
 git clone https://github.com/sjl/badwolf.git
