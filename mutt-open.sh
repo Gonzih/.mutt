d2686e1572dd36166d560d36624ff73f461a8234
Max Gonzih <gonzih@gmail.com>
2014-01-07 14:08:08 +0300

    Update mailcap to use xdg-open

---
+++ mutt-open.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#bedug="false"
+debug="true"
+
+# the tmp directory to use.
+tmpdir="/tmp/mutt_attach_$USER"
+tmplog="$tmpdir/debug.log"
+
+# make sure the tmpdir exists.
+mkdir -p $tmpdir
+
+# get rid of path
+filename=`basename $1`
+newfile=$tmpdir/$filename
+
+if [ $debug = "true" ];then
+    echo "===================" >> $tmplog
+    echo "path $1" >> $tmplog
+    echo "type $2" >> $tmplog
+    echo "open_with $3" >> $tmplog
+    echo "Filename: $filename" >> $tmplog
+    echo "New File: $newfile" >> $tmplog
+    echo "===================" >> $tmplog
+fi
+
+cp $1 $newfile
+xdg-open $newfile
