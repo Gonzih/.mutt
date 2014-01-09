64edf946d4bd06ed7eb642bde0200cc0eb547466
Max Gonzih <gonzih@gmail.com>
2014-01-09 23:03:59 +0300

    Fix theme initialization

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ ln -s ~/.mutt/muttrc ~/.muttrc
 ln -s ~/.mutt/offlineimap/offlineimaprc ~/.offlineimaprc
 cp ~/.mutt/msmtp/msmtprc ~/.msmtprc
 ln -s ~/.mutt/urlview ~/.urlview
-git clone https://github.com/sjl/badwolf.git
+git clone https://github.com/sjl/badwolf.git ~/.mutt/badwolf
