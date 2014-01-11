c6db0deefe8af67d3b1c996d8582ddde1515a38d
Max Gonzih <gonzih@gmail.com>
2014-01-11 21:57:26 +0300

    chmod msmtprc on init

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@
 ln -s ~/.mutt/muttrc ~/.muttrc
 ln -s ~/.mutt/offlineimap/offlineimaprc ~/.offlineimaprc
 cp ~/.mutt/msmtp/msmtprc ~/.msmtprc
+chmod 600 ~/.msmtprc
 ln -s ~/.mutt/urlview ~/.urlview
 git clone https://github.com/sjl/badwolf.git ~/.mutt/badwolf
